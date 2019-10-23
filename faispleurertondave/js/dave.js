436821a1d0cc1573e4e527bec98e8310046ba529
Mathilde-Garin <mathilde.garin@hotmail.fr>
2019-10-23 08:51:49 +0200

    maj OptionUp

--- faispleurertondave/js/dave.js
+++
@@ -1,4 +0,0 @@
-var VCompteurLarmes = 0;
-var VOptionUp1, VOptionUp2, VOptionUp3, VOptionUp4, VOptionUp5, VOptionUp6, VOptionUp7, VOptionUp8
-var VOptionPonc1, VOptionPonc2, VOptionPonc3, VOptionPonc4
-var VCarteMyst1, VCarteMyst2, VCarteMyst3, VCarteMyst4, VCarteMyst5, VCarteMyst6
